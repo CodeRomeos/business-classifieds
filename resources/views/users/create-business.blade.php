fe71fd7aeec23c3ae20a3926e80dd286688f7563
Gautam Kumar <guruonly007@gmail.com>
2017-06-23 16:02:23 +0530

    update create form

--- resources/views/users/create-business.blade.php
+++ resources/views/users/create-business.blade.php
@@ -9,24 +9,29 @@
 				<div class="card">
 					{{ Form::open() }}
 					<div class="form-group">
-						{{ Form::text('name', null, ['class'=>'form-control', 'placeholder'=>'Business Title']) }}
+						{{ Form::text('name', null, ['class'=>'form-control', 'placeholder'=>'Business Title', 'autofocus']) }}
 					</div>
 					<div class="form-group">
 						{{ Form::file('title', null, ['class'=>'form-control', 'placeholder'=>'Business Title']) }}
 					</div>
 					<div class="form-group row">
 						<div class="col-md-4">{{ Form::text('title', null, ['class'=>'form-control', 'placeholder'=>'Business Contact ']) }}</div>
-						<div class="col-md-4">{{ Form::text('title', null, ['class'=>'form-control', 'placeholder'=>'Business Contact (Optional..)']) }}</div>
-						<div class="col-md-4">{{ Form::text('title', null, ['class'=>'form-control', 'placeholder'=>'Business Contact (Optional..)']) }}</div>
+						<div class="col-md-4">{{ Form::text('title', null, ['class'=>'form-control', 'placeholder'=>'Business Contact (optional..)']) }}</div>
+						<div class="col-md-4">{{ Form::text('title', null, ['class'=>'form-control', 'placeholder'=>'Business Contact (optional..)']) }}</div>
 					</div>
-					<div class="form-group">
-						{{ Form::email('email', null, ['class'=>'form-control', 'placeholder'=>'Business Email']) }}
-					</div>
-					<div class="form-group">
-						{{ Form::url('website', null, ['class'=>'form-control', 'placeholder'=>'Webiste URL']) }}
+					<div class="form-group row">
+						<div class="col-md-6">
+							{{ Form::email('email', null, ['class'=>'form-control', 'placeholder'=>'Business Email']) }}
+						</div>
+						<div class="col-md-6">
+							{{ Form::email('email', null, ['class'=>'form-control', 'placeholder'=>'Business Email (optional..)']) }}
+						</div>
 					</div>
 					<div class="form-group">
 						{{ Form::textarea('website', null, ['class'=>'form-control', 'placeholder'=>'Write an amazing and unique description about your business']) }}
+					</div>
+					<div class="form-group clearfix">
+						{{ Form::submit('Submit', ['class'=>'btn btn-primary pull-right']) }}
 					</div>
 					{{ Form::close() }}
 				</div>
