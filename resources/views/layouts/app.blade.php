36dee712aca83018939af4d92d77d5e854a9ad0e
Abid Raza <ari.abid@gmail.com>
2017-07-14 18:01:19 +0530

    Install npm

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,8 +21,7 @@
         @include('web.partials.footer')
     </div>
     <!-- Scripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    {!! Html::script('js/app.js') !!}
     <script src="{{ asset('js/custom.js') }}"></script>
     <script src="{{ asset('js/trumbowyg.min.js') }}"></script>
 </body>
