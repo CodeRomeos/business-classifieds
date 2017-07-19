c2d05f631b7813d2e2c8edd3d53442d91c0a9d95
Abid Raza <ari.abid@gmail.com>
2017-07-19 18:24:42 +0530

    Remove routes

--- routes/web.php
+++ routes/web.php
@@ -15,44 +15,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
 
-Route::group(['prefix'=>'admin', 'middleware'=>'role:administrator'], function() {
-	Route::group(['prefix'=>'users'], function() {
-		Route::get('/{type}', 'UserController@adminShow')->name('adminUsers');
-	});
-	Route::group(['prefix'=>'categories'], function() {
-		Route::get('/{id}/edit', 'CategoryController@edit');
-		Route::post('/{id}/edit', 'CategoryController@update');
-		Route::post('/{id}/trash', 'CategoryController@trash');
-		Route::post('/{id}/delete', 'CategoryController@delete');
-		Route::post('/create', 'CategoryController@store');
-		Route::get('/{type?}', 'CategoryController@adminIndex')->name('adminCategories');
-	});
-	Route::get('/', 'AdminController@index')->name('adminDashboard');
-});
-
-Route::group(['prefix'=>'u', 'middleware'=>['auth','role:advertiser']], function() {
-	Route::get('/', 'UserController@dashboard')->name('userDashboard');
-	Route::get('/profile', 'UserController@profile')->name('userProfile');
-	Route::post('/profile', 'UserController@updateProfile');
-	Route::get('/password', 'UserController@password')->name('userPassword');
-	Route::post('/password', 'UserController@updatePassword');
-	Route::group(['prefix'=>'my-business'], function() {
-		Route::get('/', 'UserBusinessController@show')->name('userBusiness');
-		Route::post('/', 'UserBusinessController@update');
-		Route::post('/activation/{type}', 'UserBusinessController@updateActivation');
-	});
-});
-Route::get('/list-your-business', 'UserBusinessController@create')->name('userBusinessCreate');
-Route::post('/list-your-business', 'UserBusinessController@store');
-
-Route::group(['prefix'=>'classifieds'], function(){
-	Route::group(['prefix'=>'categories'], function(){
-		Route::get('/{slug}', 'CategoryController@show')->name('category');
-		Route::get('/', 'CategoryController@index')->name('categories');
-	});
-	Route::post('/{businessid}/reviews', 'ReviewController@storeBusinessReview')->middleware('auth');
-	Route::get('/{businessid}', 'BusinessController@show')->name('business');
-	Route::get('/', 'BusinessController@index')->name('classifieds');
-});
-
 Route::get('/', 'WelcomeController@index')->name('homepage');
